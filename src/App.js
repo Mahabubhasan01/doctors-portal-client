7476fb434ba16925feaea8831081bb575a404514
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-10 21:33:27 +0600

    first compo done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route></Route>
-        <Route></Route>
+        <Route path='login' element={<Login/>}></Route>
       </Routes>
     </>
   );
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,27 @@
+import React from "react";
+import img from  '../../assets/images/chair.png'
+
+const Banner = () => {
+  return (
+    <div className="">
+      <div classNameName="hero min-h-screen ">
+        <div className="hero-content flex-col justify-between lg:flex-row-reverse">
+          <img
+            src={img}
+            className="max-w-sm rounded-lg shadow-2xl " />
+          <div>
+            <h1 className="text-5xl font-bold">Your new smile starts here</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. <br /> In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary bg-gradient-to-r from-cyan-500 to-blue-500 uppercase font-bold text-white ">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
