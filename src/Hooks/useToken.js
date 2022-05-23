e9a61878a45c37d1d80ffa6c84095ec78e15f7f9
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-23 19:10:10 +0600

    issue

--- src/Hooks/useToken.js
+++ src/Hooks/useToken.js
@@ -6,7 +6,7 @@ const useToken = (user) => {
     const email = user?.user?.email;
     const currentUser = { email: email };
     if (email) {
-      fetch(`https://hidden-sands-99680.herokuapp.com/available/${email}`, {
+      fetch(`https://hidden-sands-99680.herokuapp.com/user/${email}`, {
         method: "PUT",
         headers: {
           "content-type": "application/json",
